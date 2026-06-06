0269a65621af3ad8b40e5fd3a3bced0c2bb0c904
Aman Choudhary <amanhoudhary4589@gmail.com>
2026-06-06 10:13:35 +0530

    Added Day 3 solutions

---
+++ Week_1/Day_4/ReshapeMatrix.java
@@ -0,0 +1,18 @@
+public class ReshapeMatrix {
+    public static int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        if (m * n != r * c) {
+            return mat;
+        }
+
+        int[][] result = new int[r][c];
+
+        for (int i = 0; i < m * n; i++) {
+            result[i / c][i % c] = mat[i / n][i % n];
+        }
+
+        return result;
+    }
+}
