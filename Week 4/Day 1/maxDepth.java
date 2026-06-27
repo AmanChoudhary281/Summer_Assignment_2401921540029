5268760b333cf840554ab7c47bab60e025c8097c
Aman Choudhary <amanhoudhary4589@gmail.com>
2026-06-27 13:29:21 +0530

    Implementation of maximum depth

---
+++ Week 4/Day 1/maxDepth.java
@@ -0,0 +1,8 @@
+import javax.swing.tree.TreeNode;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
