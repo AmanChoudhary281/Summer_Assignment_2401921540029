5824ed7d2e0f131b70b5dbd181cb9e451ff92a64
Aman Choudhary <amanhoudhary4589@gmail.com>
2026-06-27 13:44:44 +0530

    Day 5 updated

---
+++ Week 4/Day 5/Codec.java
@@ -0,0 +1,37 @@
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        helper(root, sb);
+        return sb.toString();
+    }
+
+    void helper(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(root.val).append(",");
+        helper(root.left, sb);
+        helper(root.right, sb);
+    }
+
+    public TreeNode deserialize(String data) {
+        Queue<String> q = new LinkedList<>(Arrays.asList(data.split(",")));
+        return build(q);
+    }
+
+    TreeNode build(Queue<String> q) {
+        String s = q.poll();
+
+        if (s.equals("null"))
+            return null;
+
+        TreeNode root = new TreeNode(Integer.parseInt(s));
+        root.left = build(q);
+        root.right = build(q);
+
+        return root;
+    }
+}
